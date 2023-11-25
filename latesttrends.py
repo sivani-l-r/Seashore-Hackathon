352763ed0a9f553f2415e6acbc963d7decc148e5
Sivani L R <109839211+sivani-l-r@users.noreply.github.com>
2023-11-25 15:23:10 +0530

    Update latesttrends.py

--- latesttrends.py
+++ latesttrends.py
@@ -4,12 +4,12 @@ import pandas as pd
 
 import openai
 
-openai.api_key = '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'
+openai.api_key = ''
 
 pytrends = TrendReq(hl='en-US', tz=360)
 
 
-# Get Related Queries people are searching.
+
 kw_list = ["machine learning"]
 pytrends.build_payload(kw_list, cat=0, timeframe='today 12-m')
 
@@ -17,14 +17,14 @@ data  = pytrends.related_queries()
 
 print(data['machine learning']['top'])
 
-# Keyword Suggestion
+
 keywords = pytrends.suggestions(keyword='Business Intelligence')
 df = pd.DataFrame(keywords)
 print(df['query'][0])
 
 
 
-# real time trending searches
+
 
 
 print(pytrends.trending_searches(pn='united_states'))
